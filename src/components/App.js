effbf1d870c1c9ea004437ebb3b0004f8d3ad577
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-07-20 12:30:10 -0500

    updating state and inputing default values

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,32 @@ import { Form, FormControl, Button } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      newDate : '',
+      birthday: '1996-06-25'
+    }
+  }
+
+  changeBirthday() {
+    console.log(this.state);
+    this.setState({birthday: this.state.newDate})
+  }
+
   render () {
     return (
       <div className="App">
         <Form inline>
           <h2>Input Your Birthday!</h2>
-          <FormControl type="date"></FormControl>
+          <FormControl
+            type="date"
+            onChange={event => this.setState({ newDate: event.target.value })}
+            >
+            </FormControl>
           {' '}
-          <Button>
+          <Button onClick={() => this.changeBirthday()}>
             Submit
           </Button>
         </Form>
